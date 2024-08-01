e617355ba5423b1ffa10630190e964c7fa7cc95d
Vishwas Joshi <vishwas.j@iitgn.ac.in>
2024-08-01 23:10:44 +0530

    xdictionary completed

---
+++ src/components/Dictionary.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+// import data from './data.js'
+
+const Dictionary = () => {
+    const initialDictionary=[
+
+        { word: "React", meaning: "A JavaScript library for building user interfaces." },
+    
+        { word: "Component", meaning: "A reusable building block in React." },
+    
+        { word: "State", meaning: "An object that stores data for a component." }
+    ]
+    const [dictionary]=useState(initialDictionary);
+    const [searchTerm,setSearchTerm]=useState("");
+    const [defination,setDefination]=useState("");
+
+    const handleSearch=()=>{
+        const res=dictionary.find(item=>item.word.toLowerCase()===searchTerm.toLocaleLowerCase());
+        if(res){
+            setDefination(res.meaning);
+        }
+        else{
+            setDefination("Word not found in the dictionary.");
+        }
+    }
+  return (
+    <div>
+        <h1>Dictionary App</h1>
+        <input 
+            type="text" 
+            value={searchTerm} 
+            placeholder='Enter Word'
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            ></input>
+            <button onClick={handleSearch}>
+                Search
+            </button>
+            <p>Defination</p>
+            <p>{defination}</p>
+    </div>
+  )
+}
+
+export default Dictionary
